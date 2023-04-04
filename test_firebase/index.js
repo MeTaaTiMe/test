c8e34a58496a2384b9eff6e7e25a844fa4974e98
MeTaaTiMe <metatimekun@gmail.com>
2023-04-04 17:23:20 +0900

    test firebase

--- test_firebase/index.js
+++ test_firebase/index.js
@@ -1,5 +1,1 @@
-const database = firebase.database();
 document.getElementById("btn").addEventListener("click",firebase_send());
-function firebase_send(){
-    database.ref(document.getElementById("name").value).set({txt:document.getElementById("txt").value});
-}
