397746c8febf53fd9f0bbf12138a407f2fe1aa99
MeTaaTiMe <metatimekun@gmail.com>
2023-04-04 18:26:14 +0900

    test firebase

--- test_firebase/index.js
+++ test_firebase/index.js
@@ -1,7 +1,17 @@
 const database = firebase.database();
+
 document.getElementById("btn").addEventListener("click",firebase_send);
 function firebase_send(){
     let text=document.getElementById("txt").value
-    database.ref(document.getElementById("name").value).set({txt:text});
+    database.ref(document.getElementById("name").value).set(text);
     console.log("send");
+}
+
+document.getElementById("readbtn").addEventListener("click",firebase_read);
+function firebase_read(){
+    let text=database.ref(document.getElementById("key").value).on('value', (snapshot) => {
+        const data = snapshot.val();
+        updateStarCount(postElement, data);
+    }).val;
+    document.getElementById("readtext").innerText=text;
 }
