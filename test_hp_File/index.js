c6c949fe0dad37b7fb18fe4bd8d3b6b8fe7292b5
MeTaaTiMe <metatimekun@gmail.com>
2023-04-04 16:51:13 +0900

    test firebase

---
+++ test_hp_File/index.js
@@ -0,0 +1,5 @@
+const database = firebase.database();
+document.getElementById("btn").addEventListener("click",firebase_send());
+function firebase_send(){
+    database.ref(document.getElementById("name").value).set({txt:document.getElementById("txt").value});
+}
