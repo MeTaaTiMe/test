e9763383d2a8dead863b47c950fe41205dab6b98
MeTaaTiMe <metatimekun@gmail.com>
2023-04-04 17:45:08 +0900

    test firebase

---
+++ test_firebase/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD63_9R_cZs4ZiWzI_EDiZoxwmUdLNR6BA",
+    authDomain: "test-b93d5.firebaseapp.com",
+    databaseURL: "https://test-b93d5-default-rtdb.firebaseio.com",
+    projectId: "test-b93d5",
+    storageBucket: "test-b93d5.appspot.com",
+    messagingSenderId: "946600552846",
+    appId: "1:946600552846:web:b623c13bab636fe0995cdf",
+    measurementId: "G-T2KRRR62X2"
+  };
+  firebase.initializeApp(firebaseConfig);
--- test_firebase/index.js
+++ test_firebase/index.js
@@ -1,1 +1,6 @@
+const database = firebase.database();
 document.getElementById("btn").addEventListener("click",firebase_send());
+function firebase_send(){
+    database.ref(document.getElementById("name").value).set({txt:document.getElementById("txt").value});
+    console.log("send");
+}
